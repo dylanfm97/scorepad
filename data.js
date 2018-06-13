322ef5e6d6d1b3cf43ea651fc440ee3021790ec1
Dylan Martinez <dylanmartinez@Dylans-MacBook-Air.local>
2018-06-13 11:52:47 -0600

    switched over to using vue

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = {
+	show_start_page: true,
+	show_create_game_page: false,
+	show_create_players_page: false,
+	game_input: '',
+	players_input: '',
+	rounds_input: '',
+	unlimited_check_box: false,
+	highest_score: false
+	
+}
+
+export default data
